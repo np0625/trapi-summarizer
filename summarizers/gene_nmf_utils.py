1e33021d3ab7d542d3e5d374ede2db96ec3da37c
Marc Duby <mduby@broadinstitute.org>
2025-08-15 16:05:23 -0400

    code: added sequence workflow

--- summarizers/gene_nmf_utils.py
+++ summarizers/gene_nmf_utils.py
@@ -7,8 +7,65 @@
 
 
 # methods
+def get_genes_from_trapi(string_msg, log=False):
+    '''
+    extracts a list of genes from a valid trapi response
+    '''
+    # initialize
+    list_result = []
 
+    # get gene from trapi
+
+    # return
+    return list_result
+
+
+def call_nmf_service(list_genes, log=False):
+    '''
+    calls the gene nmf service
+    '''
+    # initialize
+    json_result = {}
+
+    # call REST service
+
+    # get data
+
+    # return
+    return json_result
+
+
+def get_genes_from_nmf(json_nmf, log=False):
+    '''
+    will parse a valid nmf json and return the gene list
+    '''
+    # initialize
+    list_result = []
+
+    # get gene list from the nmf json
+
+    # return
+    return list_result
+
+
+def get_gene_set_groupings_from_nmf(json_nmf, log=False):
+    '''
+    will parse a valid nmf json and return a list of gene set groupings (by factor)
+    '''
+    # initialize
+    list_result = []
+
+    # get gene list from the nmf json
+
+    # return
+    return list_result
 
 
 
 # main
+if __name__ == "__main__":
+    '''
+    main area for tests of the above mehods
+    can be moved to pytest class later
+    '''
+    pass
