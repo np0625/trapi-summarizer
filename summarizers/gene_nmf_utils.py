d171445857da50b89d089a9820087a1619eb21d3
Marc Duby <mduby@broadinstitute.org>
2025-08-17 11:09:41 -0400

    comp: extract genes from trapi result

--- summarizers/gene_nmf_utils.py
+++ summarizers/gene_nmf_utils.py
@@ -1,23 +1,37 @@
 
 
 # imports
-
+import json
+import logging
 
 # constants
+URL_NMF = ""
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 # methods
-def get_genes_from_trapi(string_msg, log=False):
+def get_genes_from_trapi(json_trapi_result, log=False):
     '''
     extracts a list of genes from a valid trapi response
     '''
     # initialize
-    list_result = []
+    map_result = {}
+    map_nodes = {}
+
+    # get the nodes from the trapi response json
+    map_nodes = get_result_nodes(json_data=json_trapi_result, log=log)
 
     # get gene from trapi
-
+    map_result = {key: value.get('name', '') for key, value in map_nodes.items() 
+            if "biolink:Gene" in value.get("categories", [])}
+    
+    # log
+    if log:
+        logger.info("got gene map of size: {}".format(len(map_result)))
+                    
     # return
-    return list_result
+    return map_result
 
 
 def call_nmf_service(list_genes, log=False):
@@ -61,6 +75,29 @@ def get_gene_set_groupings_from_nmf(json_nmf, log=False):
     return list_result
 
 
+def get_result_nodes(json_data, log=False):
+    '''
+    extracts the node map from the result
+    '''
+    # initialize
+    map_nodes = {}
+
+    # get the nodes
+    map_nodes = json_data.get('fields', {}).get('data', {}).get('message', {}).get('knowledge_graph', {}).get('nodes', {})
+
+    # return
+    return map_nodes
+
+
+def pretty_print_json(json_data, num_lines, log=False):
+    '''
+    pretty print given number of lines fro debugging
+    '''
+    pretty_json = json.dumps(json_data, indent=2)
+    lines = pretty_json.split('\n')[:num_lines]
+    for line in lines:
+        print(line)
+
 
 # main
 if __name__ == "__main__":
@@ -68,4 +105,21 @@ if __name__ == "__main__":
     main area for tests of the above mehods
     can be moved to pytest class later
     '''
-    pass
+    # read the test result file
+    file_path = "./data/e0b7fd22-d08c-421d-a551-ea62d1417a36.json"
+    print("loading file: {}".format(file_path))    
+    with open(file_path) as json_data:
+        json_pk = json.loads(json_data.read())
+
+    # pretty print
+    # pretty_print_json(json_data=json_pk, num_lines=100)
+
+    # print the nodes json
+    json_nodes = get_result_nodes(json_data=json_pk)
+    # print(json.dumps(json_nodes, indent=2))
+
+    # get the gene map
+    map_genes = get_genes_from_trapi(json_trapi_result=json_pk, log=True)
+    pretty_print_json(json_data=map_genes, num_lines=1000)
+
+
