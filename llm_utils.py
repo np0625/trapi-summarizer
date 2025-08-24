a64b8802c42e9cd38c3d485fe309a2977f60841a
Nishad Prakash <nishad@gmail.com>
2025-08-24 14:06:28 -0400

    Clean up function param names and type hint typo

--- llm_utils.py
+++ llm_utils.py
@@ -1,12 +1,12 @@
 import json
 from pubmed_client import get_publication_info
 
-async def handle_fun_call(fun_name: str, args=str):
+async def handle_fun_call(fun_name: str, params: str):
     retval = ''
     if fun_name == 'get_publication_info':
-        arg_dict = json.loads(args)
-        pub_ids = arg_dict['pubids'].split(',')
-        retval = json.dumps(await get_publication_info(pub_ids, arg_dict['request_id']))
+        param_dict = json.loads(params)
+        pub_ids = param_dict['pubids'].split(',')
+        retval = json.dumps(await get_publication_info(pub_ids, param_dict['request_id']))
     else:
         print(f"WARNING: did not recognize #{fun_name} as a valid tool to call")
 
