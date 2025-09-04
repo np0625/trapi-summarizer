3cc389c1d221e8ee414b6d6e6144b72a0724f945
Nishad Prakash <nishad@gmail.com>
2025-09-03 22:24:11 -0400

    Utilize the ISB CT info API

--- llm_utils.py
+++ llm_utils.py
@@ -1,6 +1,7 @@
 import json
 from pubmed_client import get_publication_info
 from clinical_trials_gov_client import get_clinical_trials_gov_info
+from clinical_trials_isb_client import get_clinical_trials_isb_info
 
 async def handle_fun_call(fun_name: str, params: str):
     retval = ''
@@ -12,6 +13,10 @@ async def handle_fun_call(fun_name: str, params: str):
         param_dict = json.loads(params)
         nct_ids = param_dict['nctids']
         retval = json.dumps(await get_clinical_trials_gov_info(nct_ids))
+    elif fun_name == 'get_clinical_trials_isb_info':
+        param_dict = json.loads(params)
+        nct_ids = param_dict['nctids']
+        retval = json.dumps(await get_clinical_trials_isb_info(nct_ids))
     else:
         print(f"WARNING: did not recognize #{fun_name} as a valid tool to call")
 
